fix to data normalization

util/dataloader.py
@@ -61,7 +61,7 @@ def normalize_data(data):
         min_ch = np.min(data[:, ch])
         median_old_range = (max_ch + min_ch) / 2
         data[:, ch] = (data[:, ch] - median_old_range) / (max_ch - min_ch)
-    data += 0.01 * torch.randn(data.shape)
+    data = data + 0.01 * np.randn(data.shape)
 
 
 def split_data(dataset, split_ratio=0.1, split_type='person'):