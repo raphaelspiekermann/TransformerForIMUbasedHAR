e742058f3059ad6982ca488455a530fddc18a01a
Raphael <rs15061998@gmail.com>
2021-12-04 13:21:21 +0100

    big_transformer update

--- models/model.py
+++ models/model.py
@@ -11,4 +11,4 @@ def get_model(model_name, input_dim, output_size, window_size, n_classes, cfg):
     if model_name.lower() in ['medium', 'transformer', 'medium_transformer','medium_transformer_encoder']:
         return IMUTransformerEncoder(input_dim, output_size, window_size, n_classes, 64, 8, 128, 6, encode_postion)
     if model_name.lower() in ['big', 'big_transformer','big_transformer_encoder']:
-        return IMUTransformerEncoder(input_dim, output_size, window_size, n_classes, 512, 16, 1024, 8, encode_postion)
+        return IMUTransformerEncoder(input_dim, output_size, window_size, n_classes, 128, 16, 256, 8, encode_postion)
